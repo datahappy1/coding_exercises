4754c0a55ab9cc3982f979ec9c467998fa50bf2a
Pavel.Prudky <p.prudky@seznam.cz>
2024-03-07 17:10:01 +0100

    rewrite

--- random/jane_street_interview.py
+++ random/jane_street_interview.py
@@ -9,81 +9,84 @@ example queries:
 13 in = ? m --> answer = 0.330 (roughly)
 13 in = ? hr --> "not convertible"
 """
-from typing import List, Optional, Tuple
+from typing import Tuple
 
 
-class Fact:
-    def __init__(self, name, child_fact, child_multiplier):
-        self.name = name
-        self.child_fact = child_fact
-        self.child_multiplier = child_multiplier
+class Facts:
+    def register_fact(self, fact):
+        self.__setattr__(fact["name"], fact)
 
-    def __repr__(self):
-        return str(self.__dict__)
+    def double_link_facts(self):
+        for attr_name, attr_val in self.__dict__.items():
+            try:
+                _updated_attr = self.__dict__.get(attr_val["child"])
+                _updated_attr["parent"] = attr_name
+                _updated_attr["parent_divider"] = 1 / attr_val["child_multiplier"]
+                self.__dict__[attr_val["child"]].update(_updated_attr)
+            except TypeError:
+                pass
 
+    def _crawl_up(self, __from, __to, __source_value):
+        _anchor_fact = self.__getattribute__(__from)
+        _val = __source_value
 
-class DoubleLinkedFact(Fact):
-    def __init__(self, name, parent_fact, parent_divider, child_fact, child_multiplier):
-        super().__init__(name, child_fact, child_multiplier)
-        self.parent_fact = parent_fact
-        self.parent_divider = parent_divider
+        if _anchor_fact.get("parent") is None:
+            return None
 
+        while _anchor_fact.get("parent") != _anchor_fact["name"] and _anchor_fact["name"] != __to:
+            if _anchor_fact.get("parent_divider") is None:
+                return None
+            _val *= _anchor_fact["parent_divider"]
+            _anchor_fact = self.__getattribute__(_anchor_fact["parent"])
+            if _val > __source_value:
+                # the value increased, so clearly crawling the wrong direction
+                return None
+        return _val
 
-class LinkedFactsList:
-    def __init__(self, facts_list: List[Fact]):
-        self._facts_keyed_by_childs = {
-            fact.child_fact: fact for fact in facts_list if fact.child_fact
-        }
-        self._facts_list = [self._double_link_fact(fact) for fact in facts_list]
+    def _crawl_down(self, __from, __to, __source_value):
+        _anchor_fact = self.__getattribute__(__from)
+        _val = __source_value
 
-    def _double_link_fact(self, fact: Fact) -> DoubleLinkedFact:
-        parent_fact = self._facts_keyed_by_childs.get(fact.name)
-        return DoubleLinkedFact(
-            name=fact.name,
-            parent_fact=parent_fact.name if parent_fact else None,
-            parent_divider=1 / parent_fact.child_multiplier if parent_fact else None,
-            child_fact=fact.child_fact,
-            child_multiplier=fact.child_multiplier,
-        )
+        if _anchor_fact.get("child") is None:
+            return None
 
-    def _crawl_up(self, __from: str, __to: str, val: int) -> Optional[float]:
-        for el in self._facts_list:
-            if el.name == __from and el.name != __to:
-                val *= el.parent_divider if el.parent_divider else 1
-                return self._crawl_up(el.parent_fact, __to, val)
-            elif el.name == __from == __to:
-                return val
+        while _anchor_fact.get("child") != _anchor_fact["name"] and _anchor_fact["name"] != __to:
+            if _anchor_fact.get("child_multiplier") is None:
+                return None
+            _val *= _anchor_fact["child_multiplier"]
+            try:
+                _anchor_fact = self.__getattribute__(_anchor_fact["child"])
+            except AttributeError:
+                return None
+        return _val
 
-    def _crawl_down(self, __from: str, __to: str, val: int) -> Optional[float]:
-        for el in self._facts_list:
-            if el.name == __from and el.name != __to:
-                val *= el.child_multiplier if el.child_multiplier else 1
-                return self._crawl_down(el.child_fact, __to, val)
-            elif el.name == __from == __to:
-                return val
-
-    def crawl(self, __from: str, __to: str, val: int) -> Optional[float]:
-        return self._crawl_up(__from, __to, val) or self._crawl_down(__from, __to, val)
+    def crawl(self, __from, __to, val):
+        return self._crawl_up(__from, __to, val) or self._crawl_down(__from, __to, val) or "not compatible"
 
 
 def parse_query(query_param: str) -> Tuple[str, str, int]:
-    _qs = query_param.split(" ")
-    return _qs[1], _qs[4], int(_qs[0])
+    try:
+        _value, _query_from, _, _, _query_to = query_param.split(" ")
+        _value_numeric = int(_value)
+    except Exception as e:
+        raise ValueError(f"Cannot parse query - {e}")
+    return _query_from, _query_to, _value_numeric
 
 
 if __name__ == "__main__":
-    query = "13 in = ? hr"
-    start, end, m = parse_query(query)
-    linked_facts_list = LinkedFactsList(
-        facts_list=[
-            Fact("m", "ft", 3.28),
-            Fact("ft", "in", 12),
-            Fact("in", "mm", 25),
-            Fact("mm", None, None),
-            Fact("km", "m", 1000),
-            Fact("hr", "min", 60),
-            Fact("min", "sec", 60),
-        ]
-    )
-    result = linked_facts_list.crawl(start, end, m) or "not convertible"
+    query = "10 m = ? mm"
+    start, end, source_value = parse_query(query)
+    facts = Facts()
+    facts.register_fact({"name": "m", "child": "ft", "child_multiplier": 3.28}),
+    facts.register_fact({"name": "ft", "child": "in", "child_multiplier": 12}),
+    facts.register_fact({"name": "in", "child": "mm", "child_multiplier": 25}),
+    facts.register_fact({"name": "mm", "child": None, "child_multiplier": None}),
+    facts.register_fact({"name": "km", "child": "m", "child_multiplier": 1000}),
+    facts.register_fact({"name": "hr", "child": "min", "child_multiplier": 60}),
+    facts.register_fact({"name": "min", "child": "sec", "child_multiplier": 60}),
+    facts.double_link_facts()
+
+    # print(facts.__dict__)
+
+    result = facts.crawl(start, end, source_value) or "not convertible"
     print(f"{query} => {result}")
