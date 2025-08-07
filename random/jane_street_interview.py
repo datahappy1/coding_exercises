f6078aa03183f33802f74eeba436cf7f01c523c4
datahappy1 <p.prudky@seznam.cz>
2025-08-07 23:13:55 +0200

    adding some more random exercises

--- random/jane_street_interview.py
+++ random/jane_street_interview.py
@@ -15,75 +15,80 @@ import re
 from dataclasses import dataclass
 
 
-class CrawlDirection(Enum):
+class TraversalDirection(Enum):
     UP = "UP"
     DOWN = "DOWN"
 
 
 @dataclass
-class Fact:
+class UnitNode:
     name: str
-    child: Optional[str]
+    child: Optional["UnitNode"]
     child_multiplier: Optional[float]
-    parent: Optional[str] = None
+    parent: Optional["UnitNode"] = None
     parent_multiplier: Optional[float] = None
 
 
-class Facts:
+class UnitConverter:
     def __init__(self):
-        self._facts: Dict[str, Fact] = dict()
+        self._unit_nodes: Dict[UnitNode.name, UnitNode] = dict()
 
-    def register_fact(self, fact: Fact):
-        self._facts[fact.name] = fact
+    def register_unit_node(self, unit_node: UnitNode):
+        self._unit_nodes[unit_node.name] = unit_node
 
-    def double_link_facts(self):
-        for attr_name, attr_value in self._facts.items():
-            _child = self._facts.get(attr_value.child)
-            if not _child:  # fact without a child ( mm, sec. etc )
+    def add_parents(self):
+        for attr_name, attr_value in self._unit_nodes.items():
+            _child = self._unit_nodes.get(attr_value.child.name if attr_value.child else None)
+            if not _child:  # unit node without a child ( mm, sec. etc )
                 continue
             if attr_value.child_multiplier == 0:
                 raise ValueError(f"Invalid child multiplier for {attr_name}")
 
-            _child.parent = attr_name
+            _child.parent = attr_value
             _child.parent_multiplier = 1 / attr_value.child_multiplier
 
     @staticmethod
-    def _check_crawl_step_validity(direction: CrawlDirection, anchor_fact: Fact):
-        if (direction == CrawlDirection.UP and anchor_fact.parent is None) or (
-            direction == CrawlDirection.DOWN and anchor_fact.child is None
+    def _check_traversal_step_validity(
+        direction: TraversalDirection, anchor_unit_node: UnitNode
+    ):
+        if (direction == TraversalDirection.UP and anchor_unit_node.parent is None) or (
+                direction == TraversalDirection.DOWN and anchor_unit_node.child is None
         ):
-            # no need to crawl up if we're at the top fact in the hierarchy and vice versa
+            # no need to traverse up if we're at the top unit node in the hierarchy and vice versa
             return False
         return True
 
-    def _crawl(
-        self, from_unit: str, to_unit: str, source_value: int, direction: CrawlDirection
+    def _traverse(
+        self, from_unit: str, to_unit: str, source_value: float, direction: TraversalDirection
     ):
-        anchor_fact = self._facts.get(from_unit)
+        current_node = self._unit_nodes.get(from_unit)
         result = source_value
 
-        while anchor_fact.name != to_unit:
-            if self._check_crawl_step_validity(direction, anchor_fact) is False:
+        while current_node.name != to_unit:
+            if not self._check_traversal_step_validity(direction, current_node):
                 return None
 
-            if direction == CrawlDirection.UP:
-                result *= anchor_fact.parent_multiplier
-                anchor_fact = self._facts.get(anchor_fact.parent)
-
-            if direction == CrawlDirection.DOWN:
-                result *= anchor_fact.child_multiplier
+            if direction == TraversalDirection.UP:
+                result *= current_node.parent_multiplier
+                current_node = self._unit_nodes.get(current_node.parent.name)
+            elif direction == TraversalDirection.DOWN:
+                result *= current_node.child_multiplier
                 try:
-                    anchor_fact = self._facts.get(anchor_fact.child)
+                    current_node = self._unit_nodes.get(current_node.child.name)
                 except AttributeError:
-                    # if we ever reach child that is not declared in the facts
+                    # if we ever reach child that is not declared in the unit nodes
                     return None
 
         return result
 
-    def crawl(self, from_unit, to_unit, value):
-        return self._crawl(
-            from_unit, to_unit, value, direction=CrawlDirection.UP
-        ) or self._crawl(from_unit, to_unit, value, direction=CrawlDirection.DOWN)
+    def traverse(self, from_unit: str, to_unit: str, value: float) -> Optional[float]:
+        traverse_up_result = self._traverse(
+            from_unit, to_unit, value, direction=TraversalDirection.UP
+        )
+        traverse_down_result = self._traverse(
+            from_unit, to_unit, value, direction=TraversalDirection.DOWN
+        )
+        return traverse_up_result or traverse_down_result
 
 
 def parse_query(query_param: str) -> Tuple[str, str, float]:
@@ -97,22 +102,40 @@ def parse_query(query_param: str) -> Tuple[str, str, float]:
 
 
 if __name__ == "__main__":
-    facts = Facts()
-    facts.register_fact(Fact(name="m", child="ft", child_multiplier=3.28))
-    facts.register_fact(Fact(name="ft", child="in", child_multiplier=12))
-    facts.register_fact(Fact(name="in", child="mm", child_multiplier=25))
-    facts.register_fact(Fact(name="mm", child=None, child_multiplier=None))
-    facts.register_fact(Fact(name="sec", child=None, child_multiplier=None))
-    facts.register_fact(Fact(name="km", child="m", child_multiplier=1000))
-    facts.register_fact(Fact(name="hr", child="min", child_multiplier=60))
-    facts.register_fact(Fact(name="min", child="sec", child_multiplier=60))
-    facts.double_link_facts()
+    unit_converter = UnitConverter()
+
+    mm_node = UnitNode(name="mm", child=None, child_multiplier=None)
+    unit_converter.register_unit_node(mm_node)
+
+    inch_node = UnitNode(name="in", child=mm_node, child_multiplier=25)
+    unit_converter.register_unit_node(inch_node)
+
+    ft_node = UnitNode(name="ft", child=inch_node, child_multiplier=12)
+    unit_converter.register_unit_node(ft_node)
+
+    meter_node = UnitNode(name="m", child=ft_node, child_multiplier=3.28)
+    unit_converter.register_unit_node(meter_node)
+
+    km_node = UnitNode(name="km", child=meter_node, child_multiplier=1000)
+    unit_converter.register_unit_node(km_node)
+
+    second_node = UnitNode(name="sec", child=None, child_multiplier=None)
+    unit_converter.register_unit_node(second_node)
+
+    minute_node = UnitNode(name="min", child=second_node, child_multiplier=60)
+    unit_converter.register_unit_node(minute_node)
+
+    hr_node = UnitNode(name="hr", child=minute_node, child_multiplier=60)
+    unit_converter.register_unit_node(hr_node)
+
+    unit_converter.add_parents()
 
     queries = ["1 hr = ? sec", "2 m = ? in", "13 in = ? m", "13 in = ? hr"]
     for query in queries:
+        print(f"Solving query {query}")
         start, end, parsed_source_value = parse_query(query)
-        query_result = facts.crawl(start, end, parsed_source_value)
+        query_result = unit_converter.traverse(start, end, parsed_source_value)
         if query_result is None:
             print(f"{query} not convertible")
         else:
-            print(f"{query} => {query_result}")
+            print(f"{query} => {query_result:.3f}")
